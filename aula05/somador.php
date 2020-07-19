f8ebd37a1f95aaee228d09a56cc17c3c749f0268
AndersonGuedes07 <andersonpgca@hotmail.com>
2020-07-19 17:16:15 -0300

    aula04

---
+++ aula05/somador.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma de $n1 e $n2 é $s";
+    ?>
+</body>
+</html>
