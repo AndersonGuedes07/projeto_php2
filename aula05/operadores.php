95ae44227beddb594efa001085ed179e1d331c2f
AndersonGuedes07 <andersonpgca@hotmail.com>
2020-07-19 17:36:50 -0300

    aula05operadores

--- aula05/operadores.php
+++ aula05/operadores.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +7,16 @@
 </head>
 <body>
     <?php
-        $n1 = 2;
-        $n2 = 2;
-        echo "A soma vale ". ($n1 + $n2);
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];;
+        echo "<h1>Valores recebidos: $n1 e $n2 </h1>";
+        $m = ($n1 + $n2) / 2;
+        echo "A soma vale ". ($n1+$n2);
+        echo "<br/>A subtração vale ". ($n1-$n2);
+        echo "<br/>A multiplicação vale ". ($n1*$n2);
+        echo "<br/>A divisão vale ". ($n1/$n2);
+        echo "<br/>A modulo vale ". ($n1%$n2);
+        echo "<br/>A média vale $m";
     ?>
 </body>
 </html>
